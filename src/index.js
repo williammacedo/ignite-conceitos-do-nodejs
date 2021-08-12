19b3b8b089f52e44c206d49c3d2f3f4f4c7c9d75
William <macedo.franca@gmail.com>
2021-08-12 11:55:21 -0300

    Insert users created.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(userFind => userFind.username === username);
+
+  if (user) {
+    request.user = user;
+    return next();
+  } else {
+    return response.status(404).json({ error: 'User not found!' });
+  }
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.some(userSome => userSome.username === username);
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: 'User already exists.' });
+  }
+
+  const user = { id: uuidv4(), name, username, todos: [] }
+  users.push(user);
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
